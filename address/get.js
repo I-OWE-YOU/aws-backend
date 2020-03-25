0211c331c40f5fd5f1dac35f3e9240094eff2354
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-03-25 15:48:45 +0100

    new endpoint for processing the address

---
+++ address/get.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { failure, success } from '../libs/response-lib';
+
+export const main = async event => {
+  const { postalCode, houseNumber } = event.pathParameters;
+  const addressEndpoint = process.env.AZURE_MAPS_ADDRESS_ENDPOINT;
+  const clientId = process.env.AZURE_MAPS_CLIENT_ID;
+  const mapsKey = process.env.AZURE_MAPS_KEY;
+
+  try {
+    const response = await axios.get(
+      `${addressEndpoint}?countryCode=NL&api-version=1.0`,
+      {
+        headers: {
+          'x-ms-client-id': clientId
+        },
+        params: {
+          'subscription-key': mapsKey,
+          postalCode: postalCode.replace(' ', ''),
+          houseNumber
+        }
+      }
+    );
+
+    const data = response.data.results[0];
+
+    const address = {
+      city: data.address.localName,
+      houseNumber: houseNumber,
+      latitude: data.position.lat,
+      longitude: data.position.lon,
+      street: data.address.streetName,
+      zipCode: postalCode
+    };
+
+    return success(address);
+  } catch (e) {
+    console.log(e);
+    return failure('There has been an errpr');
+  }
+};
