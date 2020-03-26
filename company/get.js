cf13206783e7448ceff68e07e51aac7b6016c90e
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-03-26 15:25:15 +0100

    new lambda function for creating a company (#8)
    * scaffold the API endpoints for get and create a company
    
    * add dynamodb helper lib
    
    * update the create company endpoint
    
    * add JOI for validation
    
    * new error validation response
    
    * new schema for company creation
    
    * update lambda function with validation
    
    * update status code from 400 to 422
    
    * add kvk property

---
+++ company/get.js
@@ -0,0 +1,5 @@
+import { success } from '../libs/response-lib';
+
+export const main = async event => {
+  return success('Get a company');
+};
---
+++ validation/createCompanySchema.js
@@ -0,0 +1,35 @@
+import Joi from '@hapi/joi';
+
+export const createCompanySchema = Joi.object({
+  companyName: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .required(),
+
+  acceptedTerms: Joi.bool().required(),
+  copyAcceptedTerms: Joi.bool().required(),
+  email: Joi.string()
+    .email()
+    .required(),
+  contactFirstName: Joi.string().required(),
+  companyLastName: Joi.string().required(),
+  iban: Joi.string().required(),
+  kvk: Joi.number()
+    .integer()
+    .min(10000000)
+    .max(99999999)
+    .required()
+    .messages({
+      'number.min': '"kvk" should be 8 digits long',
+      'number.max': '"kvk" should be 8 digits long'
+    }),
+  address: Joi.object({
+    city: Joi.string().required(),
+    houseNumber: Joi.string().required(),
+    street: Joi.string().required(),
+    zipCode: Joi.string().required(),
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required()
+  })
+});
