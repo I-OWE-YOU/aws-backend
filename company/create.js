4b08f6137d1049679fe953601311780a119f787c
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-03-26 13:18:38 +0100

    update lambda function with validation

--- company/create.js
+++ company/create.js
@@ -1,31 +1,39 @@
 import { v1 as uuidv1 } from 'uuid';
 import * as dynamoDbLib from '../libs/dynamodb-lib';
-import { failure, success } from '../libs/response-lib';
+import { failure, success, validationError } from '../libs/response-lib';
+import { createCompanySchema } from '../validation/createCompanySchema';
 
 export const main = async event => {
+  /** @type {import('../typings/company).Company} */
   const data = JSON.parse(event.body);
+
+  try {
+    await createCompanySchema.validateAsync(data, { abortEarly: false });
+  } catch (e) {
+    console.log(e);
+    const errorMessages = e.details.map(detail => detail.message);
+    return validationError(errorMessages);
+  }
 
   const params = {
     TableName: process.env.COMPANIES_TABLE_NAME,
     Item: {
       companyId: uuidv1(),
-      // acceptedTerms: data.acceptedTerms,
-      companyName: data.companyName
-      // copyAcceptedTerms: data.copyAcceptedTerms,
-      // email: data.email,
-      // firstName: data.contactFirstName,
-      // lastName: data.companyLastName,
-      // iban: data.iban,
-      // city: data.address.city,
-      // houseNumber: data.address.houseNumber,
-      // street: data.address.street,
-      // zipCode: data.address.zipCode,
-      // latitude: data.address.latitude,
-      // longitude: data.address.longitude
+      acceptedTerms: data.acceptedTerms,
+      companyName: data.companyName,
+      copyAcceptedTerms: data.copyAcceptedTerms,
+      email: data.email,
+      firstName: data.contactFirstName,
+      lastName: data.companyLastName,
+      iban: data.iban,
+      city: data.address.city,
+      houseNumber: data.address.houseNumber,
+      street: data.address.street,
+      zipCode: data.address.zipCode,
+      latitude: data.address.latitude,
+      longitude: data.address.longitude
     }
   };
-
-  // return success(params);
 
   try {
     await dynamoDbLib.call('put', params);
@@ -34,6 +42,4 @@ export const main = async event => {
     console.log(e);
     return failure({ status: false });
   }
-
-  return success('Create a new company');
 };
---
+++ typings/company.js
@@ -0,0 +1,23 @@
+/**
+ * @typedef {Object} Company
+ * @property {boolean} acceptedTerms
+ * @property {string} companyName
+ * @property {boolean} copyAcceptedTerms
+ * @property {string} email
+ * @property {string} contactFirstName
+ * @property {string} contactLastName
+ * @property {string} iban
+ * @property {Address} address
+ */
+
+/**
+ * @typedef {object} Address
+ * @property {string} city
+ * @property {string} houseNumber
+ * @property {string} street
+ * @property {string} zipCode
+ * @property {number} latitude
+ * @property {number} longitude
+ */
+
+exports.empty = {};
