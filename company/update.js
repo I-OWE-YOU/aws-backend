72fdc6241c6da45de99bb584a569d91ed1f728ff
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-03-27 08:42:39 +0100

    new function for updating existing companies

---
+++ company/update.js
@@ -0,0 +1,88 @@
+import { failure, success, validationError } from '../libs/response-lib';
+import * as dynamoDbLib from '../libs/dynamodb-lib';
+import { companySchema } from '../validation/companySchema';
+import { flattenObject } from '../libs/utils-lib';
+
+export const main = async event => {
+  /** @type {string} companyId - UUID */
+  const companyId = event.pathParameters.id;
+
+  /** @type {import('../typings/company).Company} */
+  const data = JSON.parse(event.body);
+
+  let value = null;
+
+  try {
+    value = await companySchema('optional').validateAsync(data);
+  } catch (e) {
+    const errorMessages = e.details.map(detail => detail.message);
+    return validationError(errorMessages);
+  }
+
+  const params = {
+    TableName: process.env.COMPANIES_TABLE_NAME,
+    Key: {
+      companyId
+    },
+    // 'UpdateExpression' defines the attributes to be updated
+    // 'ExpressionAttributeValues' defines the value in the update expression
+    UpdateExpression: generateUpdateExpression(value),
+    ExpressionAttributeValues: generateExpressionAttributeValues(data),
+    // 'ReturnValues' specifies if and how to return the item's attributes,
+    // where ALL_NEW returns all attributes of the item after the update;
+    // you can inspect 'result' below to see how it works with different settings
+    ReturnValues: 'ALL_NEW'
+  };
+
+  try {
+    await dynamoDbLib.call('update', params);
+    return success({ status: true });
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+};
+
+/**
+ * Generates the update expression from the provided data object
+ *
+ * @param {import('../typings/company).CompanyFlatten} data
+ * @returns {Object}
+ * @example
+ * 'SET :prop1 = prop1, :prop2 = prop2'
+ */
+const generateUpdateExpression = data => {
+  let output = 'SET';
+  const flatten = flattenObject(data);
+  const arr = Object.keys(flatten);
+
+  arr.forEach((value, index) => {
+    const isLast = index === arr.length - 1;
+    output += ` ${value} = :${value}${isLast ? '' : ','}`;
+  });
+
+  return output;
+};
+
+/**
+ * Generates the expression attribute values from the provided object data
+ *
+ * @param {import('../typings/company).CompanyFlatten} data
+ * @returns {Object}
+ * @example
+ * {
+ *   ':prop1' = <prop1_value>,
+ *   ':prop2' = <prop2_value>
+ * }
+ */
+const generateExpressionAttributeValues = data => {
+  const output = {};
+  const flatten = flattenObject(data);
+  const arr = Object.keys(flatten);
+
+  arr.forEach(value => {
+    output[`:${value}`] = flatten[value];
+  });
+
+  return output;
+};
--- typings/company.js
+++ typings/company.js
@@ -21,4 +21,22 @@
  * @property {number} longitude
  */
 
+/**
+ * @typedef {object} CompanyFlatten
+ * @property {boolean} acceptedTerms
+ * @property {string} companyName
+ * @property {boolean} copyAcceptedTerms
+ * @property {string} email
+ * @property {string} contactFirstName
+ * @property {string} contactLastName
+ * @property {string} iban
+ * @property {number} kvk
+ * @property {string} city
+ * @property {string} houseNumber
+ * @property {string} street
+ * @property {string} zipCode
+ * @property {number} latitude
+ * @property {number} longitude
+ */
+
 exports.empty = {};
