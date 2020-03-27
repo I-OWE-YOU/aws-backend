5b032b954971697498ffcfd316914ee58f7760ad
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-03-27 11:30:48 +0100

    new endpoint for deleting a company

---
+++ company/delete.js
@@ -0,0 +1,22 @@
+import { failure, success } from '../libs/response-lib';
+import * as dynamoDbLib from '../libs/dynamodb-lib';
+
+export const main = async event => {
+  /** @type {string} companyId - UUID */
+  const companyId = event.pathParameters.id;
+
+  const params = {
+    TableName: process.env.COMPANIES_TABLE_NAME,
+    Key: {
+      companyId
+    }
+  };
+
+  try {
+    await dynamoDbLib.call('delete', params);
+    return success({ status: true });
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+};
