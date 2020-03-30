ea1ff701cde998bc77e6c07b6650ba21fbe7f5ef
Cezary Skura <cskura@mobiquityinc.com>
2020-03-30 15:55:34 +0200

    - create endpoint to get all companies

---
+++ company/getAll.js
@@ -0,0 +1,26 @@
+import { failure, resourceNotFound, success } from '../libs/response-lib';
+import * as dynamoDbLib from '../libs/dynamodb-lib';
+
+export const main = async () => {
+	const params = { TableName: process.env.COMPANIES_TABLE_NAME };
+
+	let companies = [];
+	let items;
+
+	try {
+		do {
+			items = await dynamoDbLib.call('scan', params);
+			items.Items.forEach((item) => companies.push(item));
+			params.ExclusiveStartKey = items.LastEvaluatedKey;
+		} while (typeof items.LastEvaluatedKey != "undefined");
+
+
+		if (companies.length) {
+			return success(companies);
+		} else {
+			return resourceNotFound({ status: false, error: 'Companies not found!' });
+		}
+	} catch (e) {
+		return failure({ status: false });
+	}
+};
