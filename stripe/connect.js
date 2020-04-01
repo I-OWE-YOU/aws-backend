a1af35b1d27f2bf36b00149a0c0071be0ad4a231
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-04-01 12:21:48 +0200

    add logging messages

--- stripe/connect.js
+++ stripe/connect.js
@@ -14,21 +14,27 @@ export const main = async event => {
     FilterExpression: 'userId = :userId'
   };
 
-  let company;
+  console.log(`Get the company from the logged in user with ID: ${userId}`);
 
+  let company;
   try {
     const response = await dynamoDbLib.call('scan', params);
 
     if (response.Count === 0) {
+      const error = 'resource_not_found';
+      const errorMsg = 'The company does not exist in the DB';
+      console.error({ error, errorMsg, userId });
       return resourceNotFound({ status: false });
     }
 
     company = response.Items[0];
+    console.log(`Successfully fetch the company with ID: ${company.companyId}`);
   } catch (e) {
     console.error(e);
     return failure({ status: false });
   }
 
+  console.log('Generate a unique ID');
   const uniqueToken = uuidv1();
 
   const updateParams = {
@@ -43,9 +49,17 @@ export const main = async event => {
     ReturnValues: 'ALL_NEW'
   };
 
+  console.log(
+    `Update the company with ID: ${company.companyId} with the unique ID: ${uniqueToken}`
+  );
+
   try {
     await dynamoDbLib.call('update', updateParams);
+    console.log('Successfully update company');
+
     const redirectUrl = `${process.env.STRIPE_CONNECT_URL}&client_id=${process.env.STRIPE_API_CLIENT_ID}&state=${uniqueToken}`;
+    console.log(`Generate redirect URL: ${redirectUrl}`);
+
     return success(redirectUrl);
   } catch (e) {
     console.error(e);
