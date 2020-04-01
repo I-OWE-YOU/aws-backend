365f61e8a38c4c9f56b313feffc4ebda3023124b
Cezary Skura <cskura@mobiquityinc.com>
2020-04-01 16:47:33 +0200

    fix PR comments

--- validation/createCompanySchema.js
+++ validation/createCompanySchema.js
@@ -3,7 +3,6 @@ import Joi from '@hapi/joi';
 export const companySchema = presenceMode =>
 	Joi.object({
 		email: Joi.string()
-			.email()
-			.presence(presenceMode),
+			.email(),
 		sub: Joi.string().presence(presenceMode)
 	});
