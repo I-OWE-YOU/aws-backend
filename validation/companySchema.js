1f08db1b0eb36f5ad504aa428aec02a806bae8e7
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-04-06 21:39:42 +0200

    bugfix

--- validation/companySchema.js
+++ validation/companySchema.js
@@ -13,7 +13,7 @@ export const companySchema = presenceMode =>
       .email()
       .presence(presenceMode),
     contactFirstName: Joi.string().presence(presenceMode),
-    contractInsertion: Joi.string().presence(presenceMode),
+    contactInsertion: Joi.string().presence(presenceMode),
     contractLastName: Joi.string().presence(presenceMode),
     iban: Joi.string().presence(presenceMode),
     kvk: Joi.number()
