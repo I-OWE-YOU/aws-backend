da8055201320c8d73d59906a6f09b2dcfc5c42fa
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-04-06 21:35:04 +0200

    fix company owner fields

--- validation/companySchema.js
+++ validation/companySchema.js
@@ -13,7 +13,8 @@ export const companySchema = presenceMode =>
       .email()
       .presence(presenceMode),
     contactFirstName: Joi.string().presence(presenceMode),
-    companyLastName: Joi.string().presence(presenceMode),
+    contractInsertion: Joi.string().presence(presenceMode),
+    contractLastName: Joi.string().presence(presenceMode),
     iban: Joi.string().presence(presenceMode),
     kvk: Joi.number()
       .integer()
