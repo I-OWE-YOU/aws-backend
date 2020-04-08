b540dc25ee44644e03a6f95b3c5a1abe37173444
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-04-08 11:36:53 +0200

    if there are no companies in the DB return an empty array

--- company/getAll.js
+++ company/getAll.js
@@ -1,30 +1,26 @@
-import { failure, resourceNotFound, success } from '../libs/response-lib';
+import { failure, success } from '../libs/response-lib';
 import * as dynamoDbLib from '../libs/dynamodb-lib';
 import { getEnvironment } from '../libs/utils-lib';
 
 export const main = async () => {
-	const params = {
-		TableName: getEnvironment().COMPANIES_TABLE_NAME,
-		ProjectionExpression: 'email, companyId, companyName, lastName, firstName, city, stripeUserId, longitude, iban, houseNumber, acceptedTerms, kvk, latitude, zipCode, street'
-	};
+  const params = {
+    TableName: getEnvironment().COMPANIES_TABLE_NAME,
+    ProjectionExpression:
+      'email, companyId, companyName, lastName, firstName, city, stripeUserId, longitude, iban, houseNumber, acceptedTerms, kvk, latitude, zipCode, street'
+  };
 
-	let companies = [];
-	let items;
+  let companies = [];
+  let items;
 
-	try {
-		do {
-			items = await dynamoDbLib.call('scan', params);
-			items.Items.forEach((item) => companies.push(item));
-			params.ExclusiveStartKey = items.LastEvaluatedKey;
-		} while (typeof items.LastEvaluatedKey != "undefined");
+  try {
+    do {
+      items = await dynamoDbLib.call('scan', params);
+      items.Items.forEach(item => companies.push(item));
+      params.ExclusiveStartKey = items.LastEvaluatedKey;
+    } while (typeof items.LastEvaluatedKey != 'undefined');
 
-
-		if (companies.length) {
-			return success(companies);
-		} else {
-			return resourceNotFound({ status: false, error: 'Companies not found!' });
-		}
-	} catch (e) {
-		return failure({ status: false });
-	}
+    return success(companies);
+  } catch (e) {
+    return failure({ status: false });
+  }
 };
