b40c2225d9ad69fe04add23a13600a55edd7978e
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-04-08 18:11:13 +0200

    new webhook for de-authorize the company with the platform

--- typings/stripeSecrets.js
+++ typings/stripeSecrets.js
@@ -3,6 +3,7 @@
  * @property {string} API_SECRET_KEY
  * @property {string} API_CLIENT_ID
  * @property {string} WEBHOOK_CHECKOUT_COMPLETED_SECRET_KEY
+ * @property {string} WEBHOOK_ACCOUNT_APP_DEAUTHORIZED_KEY
  **/
 
 exports.empty = {};
