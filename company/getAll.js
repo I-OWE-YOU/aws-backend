8c4a7b9210a0cb410c6c27f40499bdcc365b694b
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-04-08 19:14:47 +0200

    filter only verified companies

--- company/getAll.js
+++ company/getAll.js
@@ -6,7 +6,7 @@ export const main = async () => {
   const params = {
     TableName: getEnvironment().COMPANIES_TABLE_NAME,
     ProjectionExpression:
-      'email, companyId, companyName, lastName, firstName, city, stripeUserId, longitude, iban, houseNumber, acceptedTerms, kvk, latitude, zipCode, street'
+      'email, companyId, companyName, lastName, firstName, city, stripeUserId, longitude, iban, houseNumber, acceptedTerms, kvk, latitude, zipCode, street, isVerified'
   };
 
   let companies = [];
@@ -15,7 +15,11 @@ export const main = async () => {
   try {
     do {
       items = await dynamoDbLib.call('scan', params);
-      items.Items.forEach(item => companies.push(item));
+      items.Items.forEach(item => {
+        if (item.isVerified && item.stripeUserId.length > 0) {
+          companies.push(item);
+        }
+      });
       params.ExclusiveStartKey = items.LastEvaluatedKey;
     } while (typeof items.LastEvaluatedKey != 'undefined');
 
