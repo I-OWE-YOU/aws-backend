ccffb77b1fa7c8a30821ec48c34d3420e32dedff
Petar Blazhevski <petar.blazevski@gmail.com>
2020-04-09 09:22:59 +0200

    filter only verified companies (#78)
    * filter only verified companies
    
    * updated how the verification works (#80)
    
    * bugfix
    
    * remove the check for isVerified
    
    If the business owner has not yet verified his account, he cannot receive payouts. This is regulated by Stripe

--- company/getAll.js
+++ company/getAll.js
@@ -6,7 +6,7 @@ export const main = async () => {
   const params = {
     TableName: getEnvironment().COMPANIES_TABLE_NAME,
     ProjectionExpression:
-      'email, companyId, companyName, lastName, firstName, city, stripeUserId, longitude, iban, houseNumber, acceptedTerms, kvk, latitude, zipCode, street'
+      'email, companyId, companyName, lastName, firstName, city, stripeUserId, longitude, iban, houseNumber, acceptedTerms, kvk, latitude, zipCode, street, isVerified'
   };
 
   let companies = [];
@@ -15,12 +15,17 @@ export const main = async () => {
   try {
     do {
       items = await dynamoDbLib.call('scan', params);
-      items.Items.forEach(item => companies.push(item));
+      items.Items.forEach(item => {
+        if (item.stripeUserId) {
+          companies.push(item);
+        }
+      });
       params.ExclusiveStartKey = items.LastEvaluatedKey;
     } while (typeof items.LastEvaluatedKey != 'undefined');
 
     return success(companies);
   } catch (e) {
+    console.error(e);
     return failure({ status: false });
   }
 };
